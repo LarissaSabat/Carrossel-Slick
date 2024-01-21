f64c3e16775bf19721214d05836f4d3c5920d4fd
Larissa Batista <larissa.sabat.carvalho@gmail.com>
2024-01-20 23:29:45 -0300

    first commit

---
+++ carrossel.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.galeria').slick({
+        infinite: true,
+        speed: 500,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
